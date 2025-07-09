2e71af26daf25de7e3ec84a4b9d2634b52164e8e
Jeff <wjn9038@gmail.com>
2025-07-09 20:39:10 +0800

    Full Project

--- WpfApp1/GameState.cs
+++ WpfApp1/GameState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,142 @@ namespace WpfApp1
 
         //event - When the event happens (is triggered / raised), it automatically calls all the subscriber methods, one by one.
         public event Action<int, int> MoveMade;
+        public event Action<GameResult> GameEnded;
+        public event Action GameRestarted;
+
+        public GameState()
+        {
+            GameGrid =  new Player[3,3];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+        }
+
+        private bool CanMakeMove(int r, int c)
+        {
+            return !GameOver && GameGrid[r,c] == Player.None;
+        }
+
+        private bool IsGridFull()
+        {
+            return TurnsPassed == 9;
+        }
+
+        private void SwitchPlayer()
+        {
+            if (CurrentPlayer == Player.X)
+            {
+                CurrentPlayer = Player.O;
+            }
+            else
+            {
+                CurrentPlayer = Player.X;
+            }
+        }
+
+        //Return true if all of these squares belong to the given player; otherwise false.
+        // An array of tuples, where each tuple holds two int values.
+        private bool AreSquaresMarked((int, int)[] squares, Player player)
+        {
+            foreach ((int r, int c) in squares)
+            {
+                if (GameGrid[r,c] != player)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //out is a way to let a method give back more than one value.
+        private bool DidMoveWin(int r, int c, out WinInfo wininfo)
+        {
+            (int, int)[] row = new[] { (r, 0), (r, 1), (r, 2) };
+            (int, int)[] col = new[] { (0, c), (1, c), (2, c) };
+            (int, int)[] leftToRightDiag = new[] { (0, 0), (1, 1), (2, 2) };
+            (int, int)[] rightToLeftDiag = new[] { (2, 0), (1, 1), (0, 2) };
+
+            if (AreSquaresMarked(row, CurrentPlayer))
+            {
+                wininfo = new WinInfo { Type = WinType.Row, Number = r };
+                return true;
+            }
+
+            if (AreSquaresMarked(col, CurrentPlayer))
+            {
+                wininfo = new WinInfo { Type = WinType.Column, Number = c };
+                return true;
+            }
+
+            if (AreSquaresMarked(leftToRightDiag, CurrentPlayer))
+            {
+                wininfo = new WinInfo { Type = WinType.LeftToRightDiagonal};
+                return true;
+            }
+
+            if (AreSquaresMarked(rightToLeftDiag, CurrentPlayer))
+            {
+                wininfo = new WinInfo { Type = WinType.RighttoLeftDiagonal};
+                return true;
+            }
+
+            wininfo = null;
+            return false;
+        }
+
+        private bool DidMoveEndGame(int r, int c, out GameResult result)
+        {
+            if (DidMoveWin(r, c, out WinInfo wininfo))
+            {
+                result = new GameResult { Winner = CurrentPlayer, WinInfo = wininfo };
+                return true;
+            }
+
+            if (IsGridFull())
+            {
+                result = new GameResult { Winner = Player.None };
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        public void MakeMove(int r, int c)
+        {
+            if (!CanMakeMove(r, c))
+            {
+                return;
+            }
+
+            GameGrid[r, c] = CurrentPlayer;
+            TurnsPassed++;
+
+            if(DidMoveEndGame(r,c, out GameResult result))
+            {
+                GameOver = true;
+                /*if (MoveMade != null)
+                {
+                    MoveMade(r, c);
+                }*/
+                MoveMade?.Invoke(r, c);
+                GameEnded?.Invoke(result);
+            }
+            else
+            {
+                SwitchPlayer();
+                MoveMade?.Invoke(r, c);
+            }
+        }
+
+        public void Reset()
+        {
+            GameGrid = new Player[3, 3];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+            GameRestarted?.Invoke();
+        }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace WpfApp1
 {
@@ -20,9 +22,225 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public readonly Dictionary<Player, ImageSource> imageSource = new()
+        {
+            {Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            {Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
+        };
+
+        private readonly Dictionary<Player, ObjectAnimationUsingKeyFrames> animations = new()
+        {
+            {Player.X, new ObjectAnimationUsingKeyFrames()},
+            {Player.O, new ObjectAnimationUsingKeyFrames()} 
+        };
+
+        private readonly DoubleAnimation fadeOutAnimation = new DoubleAnimation
+        {
+            Duration = TimeSpan.FromSeconds(0.5),
+            From = 1,
+            To = 0
+        };
+
+        private readonly DoubleAnimation fadeInAnimation = new DoubleAnimation
+        {
+            Duration = TimeSpan.FromSeconds(0.5),
+            From = 0,
+            To = 1
+        };
+
+
+        private readonly Image[,] imageControls = new Image[3, 3];
+        private readonly GameState gameState = new GameState();
+        //You can’t assign gameState to another GameState later. But you can still call methods on it
+
+
         public MainWindow()
         {
             InitializeComponent();
+            SetupGameGrid();
+            SetupAnimation();
+            //subscribe the method to the event that created in the GameState.cs
+            gameState.MoveMade += OnMoveMade;
+            gameState.GameEnded += OnGameEnded;
+            gameState.GameRestarted += OnGameRestarted;
+        }
+
+        private void SetupGameGrid()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Image imageControl = new Image(); // Create a new empty Image control. This is like a blank box that can later show X or O
+                    GameGrid.Children.Add(imageControl);// Add this image control into your WPF Grid UI so it appears on screen.
+                    imageControls[r,c] = imageControl;// Save this image control into your 2D array. So later, you can update the image easily (e.g. show X or O).
+                }
+            }
+        }
+
+        private void SetupAnimation()
+        {
+            animations[Player.X].Duration = TimeSpan.FromSeconds(0.25);
+            animations[Player.O].Duration = TimeSpan.FromSeconds(0.25);
+
+            for (int i = 0; i < 16; i++)
+            {
+                Uri xUri = new Uri($"pack://application:,,,/Assets/X{i}.png");
+                BitmapImage xImg = new BitmapImage(xUri);
+                DiscreteObjectKeyFrame xKeyFrame = new DiscreteObjectKeyFrame(xImg);
+                animations[Player.X].KeyFrames.Add(xKeyFrame);
+
+                Uri oUri = new Uri($"pack://application:,,,/Assets/O{i}.png");
+                BitmapImage oImg = new BitmapImage(oUri);
+                DiscreteObjectKeyFrame oKeyFrame = new DiscreteObjectKeyFrame(oImg);
+                animations[Player.O].KeyFrames.Add(oKeyFrame);
+            }
+        }
+
+        private async Task FadeOut(UIElement uIElement)
+        {
+            uIElement.BeginAnimation(OpacityProperty, fadeOutAnimation);
+            await Task.Delay(fadeOutAnimation.Duration.TimeSpan);
+            uIElement.Visibility = Visibility.Hidden;
+        }
+
+        private async Task FadeIn(UIElement uIElement)
+        {
+            uIElement.Visibility = Visibility.Visible;
+            uIElement.BeginAnimation(OpacityProperty, fadeInAnimation);
+            await Task.Delay(fadeInAnimation.Duration.TimeSpan); 
+        }
+
+        private async Task TransitionToEndScreen(string text, ImageSource winnerImage)
+        {
+            await Task.WhenAll(FadeOut(TurnPanel), FadeOut(GameCanvas));
+            ResultText.Text = text;
+            WinnerImage.Source= winnerImage;
+            await FadeIn(EndScreen);
+        }
+
+        private async Task TransitionToGameScreen()
+        {
+            await FadeOut(EndScreen);
+            Line.Visibility = Visibility.Hidden;
+            await Task.WhenAll(FadeIn(TurnPanel), FadeIn(GameCanvas));
+        }
+
+        //Point is a struct (a small data type) that represents a position in 2D space.
+       /* public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }*/
+        private (Point,Point) FindLinePoint(WinInfo winInfo)
+        {
+            double squareSize = GameGrid.Width / 3;
+            double margin = squareSize / 2;
+
+            if (winInfo.Type == WinType.Row)
+            {
+                double y = winInfo.Number * squareSize + margin;
+                return (new Point(0,y),new Point(GameGrid.Width,y));
+            }
+
+            if (winInfo.Type == WinType.Column)
+            {
+                double x = winInfo.Number * squareSize + margin;
+                return (new Point(x,0), new Point(x, GameGrid.Height));
+            }
+            if (winInfo.Type == WinType.LeftToRightDiagonal)
+            {
+                return (new Point(0, 0), new Point(GameGrid.Width, GameGrid.Height));
+            }
+
+            return (new Point(GameGrid.Width, 0),new Point(0,GameGrid.Height));
+        }
+        
+        // async void - only for event handles, dont use unless it's an event. You cannot wait for it, cnnot catch exceptions
+        // async task - Use for everything else, Lets you await, combine tasks, handle errors
+        private async Task ShowLine(WinInfo winInfo)
+        {
+            (Point start, Point end) = FindLinePoint(winInfo);
+            Line.X1 = start.X;
+            Line.Y1 = start.Y;
+
+            DoubleAnimation x2Animation = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(0.25),
+                From = start.X,
+                To = end.X
+            };
+
+            DoubleAnimation y2Animation = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(0.25),
+                From = start.Y,
+                To = end.Y
+            };
+
+            Line.Visibility = Visibility.Visible;
+            Line.BeginAnimation(Line.X2Property,  x2Animation);
+            Line.BeginAnimation(Line.Y2Property , y2Animation);
+            await Task.Delay(x2Animation.Duration.TimeSpan);
+        }
+
+        private void OnMoveMade(int r, int c) 
+        {
+            Player player = gameState.GameGrid[r, c]; //Looks up who owns this square (X, O, or None) in your game logic.
+            imageControls[r, c].BeginAnimation(Image.SourceProperty, animations[player]); //Updates the image at that grid cell to show the right picture (X, O, or empty).
+            PlayerImage.Source = imageSource[gameState.CurrentPlayer];//Updates indicator image to show whose turn is next.
+        }
+
+        // It means this method can use the await keyword to:
+        // run something asynchronously(like a delay or a web request),
+        //pause and let other code continue,
+        //then come back and continue when it’s ready.
+        private async void OnGameEnded(GameResult result) 
+        {
+            await Task.Delay(1000);
+            if (result.Winner == Player.None)
+            {
+               await TransitionToEndScreen("It's a tie!", null);
+            }
+            else
+            {
+                await ShowLine(result.WinInfo);
+                await Task.Delay(1000);
+                await TransitionToEndScreen("Winner:", imageSource[result.Winner]);
+            }
+        }
+
+        public async void OnGameRestarted()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].BeginAnimation(Image.SourceProperty, null);
+                    imageControls[r, c].Source = null;
+                }              
+            }
+
+            PlayerImage.Source = imageSource[gameState.CurrentPlayer];
+            await TransitionToGameScreen();
+        }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            double squareSize = GameGrid.Width / 3;
+            Point clickPosition = e.GetPosition(GameGrid);
+            int row = (int)(clickPosition.Y / squareSize);
+            int col = (int)(clickPosition.X / squareSize);
+            gameState.MakeMove(row, col);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                gameState.Reset();
+            }
+            
         }
     }
 }
